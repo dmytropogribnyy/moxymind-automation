82aea9b22eaaa272bdf498a9b62a37a5b36161f0
dpogribnyy <dpogribnyy@velixo.com>
2026-05-19 11:59:31 +0200

    chore: add CI job summary and GitHub annotations for failed test

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,9 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  reporter: [['html'], ['list']],
+  reporter: process.env.CI
+    ? [['github'], ['html'], ['list']]
+    : [['html'], ['list']],
 
   projects: [
     {
