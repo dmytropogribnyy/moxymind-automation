221b5ffce8fdf500dd8dbd14af06246259b86133
Dmytro Pogribnyy <dipptrue@gmail.com>
2026-07-23 23:01:10 +0200

    test: centralize retail journey data

---
+++ test-data/products.ts
@@ -0,0 +1,13 @@
+export const products = {
+  backpack: {
+    id: 'sauce-labs-backpack',
+    name: 'Sauce Labs Backpack',
+    price: 29.99,
+  },
+} as const;
+
+export const validCustomer = {
+  firstName: 'Alex',
+  lastName: 'Morgan',
+  postalCode: '81101',
+} as const;
